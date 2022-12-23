4331c7709f78b0db51ef9eb284a4ace35a690b9a
EmreKomsal <komsal16@itu.edu.tr>
2022-12-23 15:40:21 +0300

    Initial Commit

---
+++ New Unity Project (2)/Assets/HeadTrackHandler.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class HeadTrackHandler : MonoBehaviour
+{
+    [SerializeField]
+    private DefaultTrackableEventHandler _trackableEventHandler;
+
+    [SerializeField]
+    private bool _trackable = false;
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private Vector3 _oldPos = Vector3.zero;
+
+    [SerializeField]
+    private float moveCap = 0.01f;
+
+    [SerializeField]
+    private bool trackFound = false;
+
+    [SerializeField]
+    private GameObject warningText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _oldPos = gameObject.transform.position;
+        _trackableEventHandler.OnTargetFound.AddListener(SwitchTrack);
+        _trackableEventHandler.OnTargetLost.AddListener(SwitchTrack);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_trackable && trackFound)
+        {
+            float curr_move = Vector3.Magnitude(gameObject.transform.position - _oldPos);
+
+            Debug.Log("Curr_move" + curr_move);
+
+            if (curr_move > moveCap)
+            {
+                Debug.LogWarning("Your Head Moved");
+                videoPlayer.Stop();
+                StartCoroutine(StopMoving(1f));
+            }
+            if (curr_move < moveCap)
+            {
+                videoPlayer.Play();
+            }
+
+            _oldPos = gameObject.transform.position;
+        }
+    }
+
+    public void SwitchTrackable()
+    {
+        _trackable = !_trackable;
+    }
+
+    
+    void SwitchTrack()
+    {
+        trackFound = !trackFound;
+    }
+
+    IEnumerator StopMoving(float seconds)
+    {
+        warningText.SetActive(true);
+        yield return new WaitForSeconds(seconds);
+
+        Debug.Log("Current Sound Stopped");
+        warningText.SetActive(true);
+    }
+
+
+}
---
+++ New Unity Project (2)/Assets/_Scripts/AvatarTrackHandler.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class AvatarTrackHandler : MonoBehaviour
+{
+    [SerializeField]
+    private DefaultTrackableEventHandler _trackableEventHandler;
+    [SerializeField]
+    private List<Avatar> avatars;
+    [SerializeField]
+    private Avatar selectedAvatar;
+    [SerializeField]
+    private GameObject tools;
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+    [SerializeField]
+    private HeadTrackHandler headTrackHandler;
+
+    [Header("Order")]
+    public bool isAvatarSelected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedAvatar = avatars[0];
+        _trackableEventHandler.OnTargetFound.AddListener(selectedAvatar.PlayWelcome);
+        _trackableEventHandler.OnTargetLost.AddListener(selectedAvatar.StopTalking);
+    }
+
+    public void ChangeSelectedAvatar(Avatar avatar)
+    {
+        if (!isAvatarSelected)
+        {
+            if (avatar != selectedAvatar)
+            {
+                selectedAvatar = avatar;
+            }
+            isAvatarSelected = true;
+            selectedAvatar.StopTalking();
+            selectedAvatar.PlaySelected();
+            _trackableEventHandler.OnTargetFound.RemoveListener(selectedAvatar.PlayWelcome);
+            _trackableEventHandler.OnTargetFound.AddListener(selectedAvatar.PlaySelected);
+        }
+    }
+
+    public void SelectPlayed()
+    {
+        selectedAvatar.PlayTools();
+        //tools.SetActive(true);
+        _trackableEventHandler.OnTargetFound.RemoveListener(selectedAvatar.PlaySelected);
+        _trackableEventHandler.OnTargetFound.AddListener(selectedAvatar.PlayTools);
+    }
+
+    public void ToolsPlayed()
+    {
+        _trackableEventHandler.OnTargetFound.RemoveListener(selectedAvatar.PlayTools);
+        //tools.SetActive(false);
+        _trackableEventHandler.OnTargetFound.AddListener(selectedAvatar.PlayMovie);
+        selectedAvatar.PlayMovie();
+    }
+
+    public void MoviePlayed()
+    {
+        _trackableEventHandler.OnTargetFound.RemoveListener(selectedAvatar.PlayMovie);
+        videoPlayer.gameObject.SetActive(true);
+        //headTrackHandler.IsTrackable(true);
+        _trackableEventHandler.OnTargetFound.AddListener(StartMovie);
+        _trackableEventHandler.OnTargetLost.AddListener(StopMovie);
+    }
+
+    private void StartMovie()
+    {
+        if (!videoPlayer.gameObject.activeSelf)
+        {
+            videoPlayer.gameObject.SetActive(true);
+        }
+        videoPlayer.Play();
+    }
+
+    private void StopMovie()
+    {
+        if (videoPlayer.gameObject.activeSelf)
+        {
+            videoPlayer.gameObject.SetActive(false);
+        }
+        videoPlayer.Pause();
+    }
+}
